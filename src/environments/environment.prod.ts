7c8d289a2a2d31f1f2798ef0415938124e00c12c
DESKTOP-L4LNMKF\Mudassar <mudassar.gulzar01@gmail.com>
2022-01-25 02:13:32 +0500

    init
    (cherry picked from commit c71442d8e036965bf70aa839b0744b0df0656e9b)

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,168 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  appVersion: 'v8.0.34',
+  USERDATA_KEY: 'authf649fc9a5f55',
+  isMockEnabled: true,
+  apiUrl: 'api',
+  appThemeName: 'Metronic',
+  appPurchaseUrl: 'https://1.envato.market/EA4JP',
+  appHTMLIntegration: 'https://preview.keenthemes.com/metronic8/divideals/documentation/base/helpers/flex-layouts.html',
+  appPreviewUrl: 'https://preview.keenthemes.com/metronic8/angular/divideals/',
+  appPreviewAngularUrl: 'https://preview.keenthemes.com/metronic8/angular/divideals',
+  appPreviewDocsUrl: 'https://preview.keenthemes.com/metronic8/angular/docs',
+  appPreviewChangelogUrl: 'https://preview.keenthemes.com/metronic8/angular/docs/changelog',
+  appDemos: {
+    'divideals': {
+      'title': 'Demo 1',
+      'description': 'Default Dashboard',
+      'published': true,
+      'thumbnail': './assets/media/demos/divideals.png'
+    },
+
+    'demo2': {
+      'title': 'Demo 2',
+      'description': 'SaaS Application',
+      'published': false,
+      'thumbnail': './assets/media/demos/demo2.png'
+    },
+
+    'demo3': {
+      'title': 'Demo 3',
+      'description': 'New Trend',
+      'published': false,
+      'thumbnail': './assets/media/demos/demo3.png'
+    },
+
+    'demo4': {
+      'title': 'Demo 4',
+      'description': 'Intranet Application',
+      'published': false,
+      'thumbnail': './assets/media/demos/demo4.png'
+    },
+
+    'demo5': {
+      'title': 'Demo 5',
+      'description': 'Support Forum',
+      'published': false,
+      'thumbnail': './assets/media/demos/demo5.png'
+    },
+
+    'demo6': {
+      'title': 'Demo 6',
+      'description': 'Admin Backend',
+      'published': false,
+      'thumbnail': './assets/media/demos/demo6.png'
+    },
+
+    'demo7': {
+      'title': 'Demo 7',
+      'description': 'CRM Dashboard',
+      'published': false,
+      'thumbnail': './assets/media/demos/demo7.png'
+    },
+
+    'demo8': {
+      'title': 'Demo 8',
+      'description': 'Core Dashboard',
+      'published': false,
+      'thumbnail': './assets/media/demos/demo8.png'
+    },
+
+    'demo9': {
+      'title': 'Demo 9',
+      'description': 'Fancy Dashboard',
+      'published': false,
+      'thumbnail': './assets/media/demos/demo9.png'
+    },
+
+    'divideals0': {
+      'title': 'Demo 10',
+      'description': 'Modern Dashboard',
+      'published': false,
+      'thumbnail': './assets/media/demos/divideals0.png'
+    },
+
+    'divideals1': {
+      'title': 'Demo 11',
+      'description': 'Light Dashboard',
+      'published': false,
+      'thumbnail': './assets/media/demos/divideals1.png'
+    },
+
+    'divideals2': {
+      'title': 'Demo 12',
+      'description': 'Reporting System',
+      'published': false,
+      'thumbnail': './assets/media/demos/divideals2.png'
+    },
+
+    'divideals3': {
+      'title': 'Demo 13',
+      'description': 'Classic Dashboard',
+      'published': false,
+      'thumbnail': './assets/media/demos/divideals3.png'
+    },
+
+    'divideals4': {
+      'title': 'Demo 14',
+      'description': 'Creative Dashboard',
+      'published': false,
+      'thumbnail': './assets/media/demos/divideals4.png'
+    },
+
+    'divideals5': {
+      'title': 'Demo 15',
+      'description': 'Minimalistic Dashboard',
+      'published': false,
+      'thumbnail': './assets/media/demos/divideals5.png'
+    },
+
+    'divideals6': {
+      'title': 'Demo 16',
+      'description': 'Modern Dashboard',
+      'published': false,
+      'thumbnail': './assets/media/demos/divideals6.png'
+    },
+
+    'divideals7': {
+      'title': 'Demo 17',
+      'description': 'Backend System',
+      'published': false,
+      'thumbnail': './assets/media/demos/divideals7.png'
+    },
+
+    'divideals8': {
+      'title': 'Demo 18',
+      'description': 'System Dashboard',
+      'published': false,
+      'thumbnail': './assets/media/demos/divideals8.png'
+    },
+
+    'divideals9': {
+      'title': 'Demo 19',
+      'description': 'Light Dashboard',
+      'published': false,
+      'thumbnail': './assets/media/demos/divideals9.png'
+    },
+
+    'demo20': {
+      'title': 'Demo 20',
+      'description': 'Monochrome Dashboard',
+      'published': false,
+      'thumbnail': './assets/media/demos/demo20.png'
+    },
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
---
+++ tailwind.config.js
@@ -0,0 +1,75 @@
+const primaryColors = {
+  light: '#F7E1AD',
+  DEFAULT: '#E9A608',
+  dark: '#E67B00',
+};
+
+module.exports = {
+  prefix: '',
+  purge: {
+    enabled: process.env.TAILWIND_MODE === 'build',
+    content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
+  },
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'primary-100': '#F7E1AD',
+        'primary-400': '#E9A608',
+      },
+      width: {
+        '35': '35%',
+        '38': '38%',
+        '62': '62%',
+        '65': '65%',
+      }
+    },
+    fontFamily: {
+      display: ['roboto', 'sans-serif'],
+      body: ['roboto', 'sans-serif'],
+    },
+    container: {
+      center: true,
+      padding: '1.5rem',
+    },
+    extends: {
+      color: {
+        inherit: 'inherit',
+        transparent: 'transparent',
+        current: 'currentColor',
+      },
+    },
+    linearBorderGradients: {
+      directions: {
+        // defaults to these values
+        dp: '109deg',
+        t: 'to top',
+        tr: 'to top right',
+        r: 'to right',
+        br: 'to bottom right',
+        b: 'to bottom',
+        bl: 'to bottom left',
+        l: 'to left',
+        tl: 'to top left',
+      },
+      colors: {
+        p: ['#59C8D1 0%', '#75F16D 100%'],
+      },
+      background: {
+        'bg': primaryColors.DEFAULT,
+        'bg-light': primaryColors.light,
+        'bg-dark': primaryColors.dark,
+      },
+      border: {
+        // defaults to these values (optional)
+        1: '1px',
+        2: '2px',
+        4: '4px',
+      },
+    },
+  },
+  variants: {
+    linearBorderGradients: ['responsive', 'hover', 'dark'], // defaults to ['responsive']
+  },
+  plugins: [],
+};
