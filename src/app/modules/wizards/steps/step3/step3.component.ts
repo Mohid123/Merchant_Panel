de571b321a1be6926168f5c6d43f88b003f6e3cc
ijunaidkhan <junaid.mehmood@solissol.com>
2022-02-10 15:56:45 +0500

    step 1 and 3 final

--- src/app/modules/wizards/steps/step3/step3.component.ts
+++ src/app/modules/wizards/steps/step3/step3.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Subscription } from 'rxjs';
 import { ICreateAccount } from '../../create-account.helper';
 
@@ -17,45 +18,51 @@ export class Step3Component implements OnInit {
 
   private unsubscribe: Subscription[] = [];
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private modalService: NgbModal) {}
+
+  openVerticallyCentered(content: any) {
+    this.modalService.open(content, { centered: true });
+  }
 
   ngOnInit() {
-    this.initForm();
-    this.updateParentModel({}, this.checkForm());
+    // this.initForm();
+    // this.updateParentModel({}, this.checkForm());
   }
 
-  initForm() {
-    this.form = this.fb.group({
-      businessName: [this.defaultValues.businessName, [Validators.required]],
-      businessDescriptor: [
-        this.defaultValues.businessDescriptor,
-        [Validators.required],
-      ],
-      businessType: [this.defaultValues.businessType, [Validators.required]],
-      businessDescription: [this.defaultValues.businessDescription],
-      businessEmail: [
-        this.defaultValues.businessEmail,
-        [Validators.required, Validators.email],
-      ],
-    });
 
-    const formChangesSubscr = this.form.valueChanges.subscribe((val) => {
-      this.updateParentModel(val, this.checkForm());
-    });
-    this.unsubscribe.push(formChangesSubscr);
-  }
 
-  checkForm() {
-    return !(
-      this.form.get('businessName')?.hasError('required') ||
-      this.form.get('businessDescriptor')?.hasError('required') ||
-      this.form.get('businessType')?.hasError('required') ||
-      this.form.get('businessEmail')?.hasError('required') ||
-      this.form.get('businessEmail')?.hasError('email')
-    );
-  }
+  // initForm() {
+  //   this.form = this.fb.group({
+  //     businessName: [this.defaultValues.businessName, [Validators.required]],
+  //     businessDescriptor: [
+  //       this.defaultValues.businessDescriptor,
+  //       [Validators.required],
+  //     ],
+  //     businessType: [this.defaultValues.businessType, [Validators.required]],
+  //     businessDescription: [this.defaultValues.businessDescription],
+  //     businessEmail: [
+  //       this.defaultValues.businessEmail,
+  //       [Validators.required, Validators.email],
+  //     ],
+  //   });
 
-  ngOnDestroy() {
-    this.unsubscribe.forEach((sb) => sb.unsubscribe());
-  }
+  //   const formChangesSubscr = this.form.valueChanges.subscribe((val) => {
+  //     this.updateParentModel(val, this.checkForm());
+  //   });
+  //   this.unsubscribe.push(formChangesSubscr);
+  // }
+
+  // checkForm() {
+  //   return !(
+  //     this.form.get('businessName')?.hasError('required') ||
+  //     this.form.get('businessDescriptor')?.hasError('required') ||
+  //     this.form.get('businessType')?.hasError('required') ||
+  //     this.form.get('businessEmail')?.hasError('required') ||
+  //     this.form.get('businessEmail')?.hasError('email')
+  //   );
+  // }
+
+  // ngOnDestroy() {
+  //   this.unsubscribe.forEach((sb) => sb.unsubscribe());
+  // }
 }
