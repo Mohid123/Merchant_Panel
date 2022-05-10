56d89d5a3ad4d28c3243991fde3a3698d7455dad
Mohid123456 <mohid.dastgeer@solissol.com>
2022-05-10 11:15:13 +0500

    small fix

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -58,6 +58,7 @@
  * Zone JS is required by default for Angular itself.
  */
 // import '@angular/localize/init';
+import '@angular/localize/init';
 import 'zone.js'; // Included with Angular CLI.
 
 
