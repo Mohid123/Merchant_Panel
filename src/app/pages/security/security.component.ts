589b6eff33d40c7834d07fb6e37237666d3e3799
Mohid123 <muhammadmohid141@gmail.com>
2022-05-26 22:14:19 +0500

    security design

--- src/app/pages/security/security.component.ts
+++ src/app/pages/security/security.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from 'src/app/modules/auth/components/reset-password/custom-validators';
+import { ConfirmedValidator } from 'src/app/modules/auth/components/reset-password/password.validator';
 
 @Component({
   selector: 'app-security',
@@ -7,9 +10,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecurityComponent implements OnInit {
 
-  constructor() { }
+  passwordHide: boolean = true;
+
+  passForm: FormGroup = this.fb.group({
+
+    oldPass: [
+      '',
+      Validators.compose([
+        Validators.required
+      ])
+    ],
+
+    password: [
+      '',
+      Validators.compose([
+        CustomValidators.patternValidator(/\d/, {
+          hasNumber: true
+        }),
+        CustomValidators.patternValidator(/[A-Z]/, {
+          hasCapitalCase: true
+        }),
+        CustomValidators.patternValidator(/[a-z]/, {
+          hasSmallCase: true
+        }),
+        CustomValidators.patternValidator(
+          /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/,
+          {
+            hasSpecialCharacters: true
+          }
+        ),
+        Validators.minLength(8),
+        Validators.required
+      ])
+    ],
+
+    confirmPass: [
+      '',
+      Validators.compose([
+        Validators.required
+      ]),
+    ],
+    }, {
+      validator: ConfirmedValidator('password', 'confirmPass')
+  })
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  passwordShowHide(): void {
+    this.passwordHide = !this.passwordHide;
   }
 
 }
