e2812823451fb5b09da3de5f8d77474a7776181d
Mohid123 <muhammadmohid141@gmail.com>
2022-06-20 13:58:49 +0500

    step 4 start

--- src/app/modules/wizards/steps/step4/step4.component.ts
+++ src/app/modules/wizards/steps/step4/step4.component.ts
@@ -1,28 +1,26 @@
-import { ChangeDetectorRef, Component, Input, OnInit, ViewChild } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
-import { MediaService } from '@core/services/media.service';
-import { CalendarOptions, DateSelectArg, EventApi, EventClickArg } from '@fullcalendar/angular';
-import { HotToastService } from '@ngneat/hot-toast';
-import * as moment from 'moment';
-import { combineLatest, of, Subscription } from 'rxjs';
-import { exhaustMap, take } from 'rxjs/operators';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { User } from '@core/models/user.model';
+import { Subject, Subscription } from 'rxjs';
+import { AuthService } from 'src/app/modules/auth';
 import { ReusableModalComponent } from 'src/app/_metronic/layout/components/reusable-modal/reusable-modal.component';
+import { ModalConfig } from '../../../../@core/models/modal.config';
 import { MainDeal } from '../../models/main-deal.model';
-import { ModalConfig } from './../../../../@core/models/modal.config';
-import { DealService } from './../../../../@core/services/deal.service';
-import { ConnectionService } from './../../services/connection.service';
-import { createEventId } from './event-utils';
+import { ConnectionService } from '../../services/connection.service';
+
+enum CheckBoxType { ONE, TWO, NONE };
 
 @Component({
   selector: 'app-step4',
   templateUrl: './step4.component.html',
 })
-export class Step4Component implements OnInit {
+export class Step4Component implements OnInit, OnDestroy {
 
-  @Input() images: Array<any>;
+  check_box_type = CheckBoxType;
+  currentlyChecked: CheckBoxType;
+
   @ViewChild('modal') private modal: ReusableModalComponent;
-  uploaded: boolean;
 
   public modalConfig: ModalConfig = {
     onDismiss: () => {
@@ -35,153 +33,248 @@ export class Step4Component implements OnInit {
     closeButtonLabel: "Close"
   }
 
-  currentEvents: EventApi[] = [];
-  calendarOptions: CalendarOptions = {
-    headerToolbar: {
-      left: 'prev,next today',
-      center: 'title',
-      right: ''
-    },
-    // height: "auto",
-    initialView: 'dayGridMonth',
-    weekends: true,
-    editable: true,
-    selectable: true,
-    selectMirror: true,
-    dayMaxEvents: 3,
-    firstDay: 1,
-    height: 'auto',
-    validRange: {
-      start: moment().format('YYYY-MM-DD')
-    },
-    select: this.handleDateSelect.bind(this),
-    eventClick: this.handleEventClick.bind(this),
-    eventsSet: this.handleEvents.bind(this)
-  };
+  btnDisable: boolean = true;
+  editable: boolean = false
+
+  @Output() nextClick = new EventEmitter();
+  @Output() prevClick = new EventEmitter();
 
   @Input('updateParentModel') updateParentModel: (
     part: Partial<MainDeal>,
     isFormValid: boolean
   ) => void;
+
+  config: any;
+
+  public Editor = ClassicEditor
+  public isDisabled: boolean = true;
+
   form: FormGroup;
-  @Input() deal: Partial<MainDeal>;
+  policyForm: FormGroup;
+  destroy$ = new Subject();
+
+  @Input() deal: Partial<MainDeal> = {
+    vouchers: [
+      {
+        voucherEndDate: '',
+        voucherStartDate: '',
+        voucherValidity: 0
+      }
+    ]
+  };
 
   reciever: Subscription;
   data: MainDeal;
+  policy: Partial<User>;
+  editIndex: number = -1;
 
   private unsubscribe: Subscription[] = [];
 
-  constructor(
-    private connection: ConnectionService,
-    private dealService: DealService,
-    private mediaService: MediaService,
-    private router: Router,
-    private cf: ChangeDetectorRef,
-    private toast: HotToastService
-  ) {
+  constructor(private fb: FormBuilder, private connection: ConnectionService, private authService: AuthService) {
     this.reciever = this.connection.getData().subscribe((response: MainDeal) => {
-      this.data = response
+      if(response) {
+        this.data = response;
+        //console.log(this.data);
+      }
     })
-    this.uploaded = true;
   }
 
   ngOnInit() {
+    this.authService.retreiveUserPolicy();
+    this.initDateForm();
+    this.initPolicyForm();
     this.updateParentModel({}, true);
+    this.policy = this.authService.userPolicy;
+
+    if(this.form.get('voucherStartDate')?.value) {
+      this.currentlyChecked = this.check_box_type.ONE
+    }
+
+    if(!this.form.controls['voucherValidity'].value) {
+      this.form.controls['voucherValidity'].disable();
+      this.form.controls['voucherValidity'].setValue(0);
+    }
+
+    // if(this.form.get('voucherValidity')?.value) {
+    //   this.currentlyChecked = this.check_box_type.TWO
+    // }
+
+    this.config = {
+      language: 'en',
+      toolbar: {
+        styles: [
+            'alignLeft', 'alignCenter', 'alignRight', 'full', 'side'
+            ],
+        items: [
+          'heading',
+          'fontSize',
+          'bold',
+          'italic',
+          'underline',
+          'highlight',
+          'alignment',
+          'indent',
+          'outdent',
+          'undo',
+          'redo'
+        ]
+      }
+    }
   }
 
-  handleWeekendsToggle() {
-    const { calendarOptions } = this;
-    calendarOptions.weekends = !calendarOptions.weekends;
+  selectCheckBox(targetType: CheckBoxType) {
+    if(this.currentlyChecked === targetType) {
+      this.currentlyChecked = CheckBoxType.NONE;
+      return;
+    }
+    this.currentlyChecked = targetType;
   }
 
-  handleDateSelect(selectInfo: DateSelectArg) {
-    // console.log('moment().isSame(selectInfo.startStr):',moment().isSame(selectInfo.startStr,'day'));
-    // if(!moment().isSame(selectInfo.startStr,'day') && moment().isAfter(selectInfo.startStr)) { return }
+  disableCheckBox() {
 
-    const title = this.data.dealHeader
-    const calendarApi = selectInfo.view.calendar;
+    if(this.currentlyChecked == this.check_box_type.ONE) {
+      this.btnDisable = true;
+      this.form.controls['voucherValidity'].disable();
+      this.form.get('voucherValidity')?.setValue(0);
+      this.form.controls['voucherStartDate'].enable();
+      this.form.controls['voucherEndDate'].enable();
+    }
+    else if(this.currentlyChecked == this.check_box_type.TWO) {
+      this.btnDisable = false;
+      this.form.controls['voucherValidity'].enable();
+      this.form.get('voucherStartDate')?.setValue('');
+      this.form.get('voucherEndDate')?.setValue('');
+      this.form.controls['voucherStartDate'].disable();
+      this.form.controls['voucherEndDate'].disable();
+    }
+  }
 
-    calendarApi.unselect(); // clear date selection
+  initPolicyForm() {
+    this.policyForm = this.fb.group({
+      streetAddress: [
+        '',
+        Validators.compose([
+          Validators.required
+        ])
+      ],
+      zipCode: [
+        '',
+        Validators.compose([
+          Validators.required
+        ])
+      ],
+      city: [
+        '',
+        Validators.compose([
+          Validators.required
+        ])
+      ],
+      province: [
+        '',
+        Validators.compose([
+          Validators.required
+        ])
+      ]
+    })
+  }
 
-    if (title && !calendarApi.getEvents().length) {
-      this.data.startDate = selectInfo.startStr;
-      this.data.endDate = selectInfo.endStr;
-      calendarApi.addEvent({
-        id: createEventId(),
-        title,
-        start: selectInfo.startStr,
-        end: selectInfo.endStr,
-        allDay: selectInfo.allDay
+  toggleDisabled() {
+    if(this.form.controls['termsAndCondition']?.disabled) {
+      this.form.controls['termsAndCondition']?.enable();
+    }
+    else {
+      this.form.controls['termsAndCondition']?.disable();
+    }
+  }
+
+  initDateForm() {
+    this.form = this.fb.group({
+      voucherStartDate: [
+        this.data.vouchers[0]?.voucherStartDate,
+        Validators.compose([
+          Validators.required
+        ])
+      ],
+      voucherEndDate: [
+        this.data.vouchers[0]?.voucherEndDate,
+        Validators.compose([
+          Validators.required
+        ])
+      ],
+      termsAndCondition: [
+        {value: this.data.termsAndCondition, disabled: true},
+        Validators.compose([
+          Validators.required
+        ])
+      ],
+      voucherValidity: [
+        this.data.vouchers[0]?.voucherValidity,
+        Validators.compose([
+          Validators.required
+        ])
+      ]
+    });
+
+    const formChangesSubscr = this.form.valueChanges.subscribe((val) => {
+      this.data.vouchers?.forEach((voucher) => {
+        if (val.voucherValidity) {
+          voucher.voucherValidity = val.voucherValidity;
+          voucher.voucherStartDate ='';
+          voucher.voucherEndDate ='';
+        } else {
+          voucher.voucherValidity = 0;
+          voucher.voucherStartDate = val.voucherStartDate;
+          voucher.voucherEndDate = val.voucherEndDate;
+        }
+      });
+      this.updateParentModel(this.data, true);
+      this.connection.sendData(this.data);
+      console.log(this.data)
+    });
+    this.unsubscribe.push(formChangesSubscr);
+  }
+
+  checkForm() {
+    // return !(
+    //   this.form.get('voucherStartDate')?.hasError('required') ||
+    //   this.form.get('voucherEndDate')?.hasError('required') ||
+    //   this.form.get('voucherValidity')?.hasError('required')
+    // );
+  }
+
+  handleMinus() {
+    if(this.form.controls['voucherValidity'].value >= 1) {
+      this.form.patchValue({
+        voucherValidity: this.form.get('voucherValidity')?.value - 1
       });
     }
   }
 
-  handleEventClick(clickInfo: EventClickArg) {
-    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-      clickInfo.event.remove();
-      this.data.startDate = '';
-      this.data.endDate = '';
-    }
-  }
-
-  handleEvents(events: EventApi[]) {
-    this.currentEvents = events;
-    this.cf.detectChanges();
-  }
-
-  openNew() {
-    if(this.currentEvents.length == 0) {
-      this.toast.warning('Please set a date for the deal!', {
-        style: {
-          border: '1px solid #F59E0B',
-          padding: '16px',
-          color: '#F59E0B',
-        },
-        iconTheme: {
-          primary: '#f7ce8c',
-          secondary: '#F59E0B',
-        }
-      })
-      return;
-    }
-    this.connection.disabler = false;
-    this.uploaded = false;
-    const mediaUpload:any = [];
-    if(!!this.images.length) {
-      for (let index = 0; index < this.images.length; index++) {
-        mediaUpload.push(this.mediaService.uploadMedia('deal', this.images[index]));
-      }
-    }
-
-    this.data.mediaUrl = [];
-    combineLatest(mediaUpload)
-        .pipe(
-          take(1),
-          exhaustMap((mainResponse:any) => {
-            mainResponse.forEach((res:any)=> {
-              if (!res.hasErrors()) {
-                this.data.mediaUrl?.push(res.data.url);
-              } else {
-                return of(null);
-              }
-            })
-            return this.dealService.createDeal(this.data);
-          }),
-        ).subscribe(async (res) => {
-        if(!res.hasErrors()) {
-          this.uploaded = true;
-          return await this.modal.open();
-        }
-    })
-  }
-
-  async closeModal() {
-    return await this.modal.close().then(() => {
-      this.router.navigate(['/crafted/pages/wizards/view-deal'])
+  handlePlus() {
+    this.form.patchValue({
+      voucherValidity: this.form.get('voucherValidity')?.value + 1
     });
   }
 
+  async openNew() {
+    this.policyForm.patchValue(this.policy);
+    return await this.modal.open();
+  }
+
+  async closeModal() {
+    return await this.modal.close();
+  }
+
+  disableManual(e: any) {
+    e.preventDefault()
+  }
+
+
   ngOnDestroy() {
     this.unsubscribe.forEach((sb) => sb.unsubscribe());
+    this.reciever.unsubscribe();
+    this.destroy$.complete();
+    this.destroy$.unsubscribe();
   }
 }
+
