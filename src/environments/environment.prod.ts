b2bd9559bf68062dab769593f236e8e1f285fcc2
Mohid123456 <mohid.dastgeer@solissol.com>
2022-10-10 13:47:43 +0500

    change env for newApi

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
   appVersion: 'v8.0.34',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: true,
-  apiUrl: 'https://stagingdividealapi.dividisapp.com',
+  apiUrl: 'https://signupdividealapi.dividisapp.com',
   appThemeName: 'Divideals',
   appPurchaseUrl: 'https://1.envato.market/EA4JP',
   appHTMLIntegration: 'https://preview.keenthemes.com/metronic8/divideals/documentation/base/helpers/flex-layouts.html',
